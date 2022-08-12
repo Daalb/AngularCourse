dac4156a958bf2bc63875fd39c7921978cbad989
Diego Albor <dalbor@consware.com.co>
2022-08-12 11:30:27 -0500

    Search and GetOne module has been added

--- 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interrfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
-  templateUrl: './heroe.component.html'
+  templateUrl: './heroe.component.html',
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  heroe!: Heroe;
+  
+  constructor(
+    private activateRoute: ActivatedRoute,//*Para poder leer el URL en la que estoy
+    private heroesService: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(//*El observable que se obtiene en activateRoute.params se pasa a el switchMap
+        switchMap(({id}) => this.heroesService.getHeroePorId(id))
+      )
+      .subscribe(heroe => this.heroe = heroe)
   }
 
+  regresar(){
+    this.router.navigate(['./heroes/listado'])
+  } 
 }
--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interrfaces';
 
 import { HeroesService } from '../../services/heroes.service';
 
@@ -8,11 +9,12 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-      .subscribe( resp => console.log(resp))
+      .subscribe( heroes => { this.heroes = heroes })
   }
 
 }
